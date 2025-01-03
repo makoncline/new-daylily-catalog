bd6cb31bca4a6d521ca01b510c21386aa7b23167
Makon <makon@hey.com>
2025-01-02 20:57:27 -0700

    unused file

--- src/app/api/revalidate/route.ts
+++
@@ -1,7 +0,0 @@
-import { revalidatePath } from "next/cache";
-
-export async function GET() {
-  // Revalidate all dynamic routes
-  revalidatePath("/", "layout");
-  return new Response("OK", { status: 200 });
-}
