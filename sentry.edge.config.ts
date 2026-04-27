d071f2c4f06311e8f96aedbdfee52b528cb6ea30
Makon <makon@hey.com>
2026-04-27 09:49:31 -0600

    Fix Sentry runtime setup

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,13 +6,19 @@
 import * as Sentry from "@sentry/nextjs";
 
 const isSentryEnabled = process.env.NEXT_PUBLIC_SENTRY_ENABLED !== "false";
+const isProduction = process.env.NODE_ENV === "production";
+const sentryDsn =
+  process.env.SENTRY_DSN ??
+  process.env.NEXT_PUBLIC_SENTRY_DSN ??
+  "https://b3773458fec6aa0c594a9c1c73ed046a@o1136137.ingest.us.sentry.io/4508939597643776";
 
 if (isSentryEnabled) {
   Sentry.init({
-    dsn: "https://b3773458fec6aa0c594a9c1c73ed046a@o1136137.ingest.us.sentry.io/4508939597643776",
+    dsn: sentryDsn,
 
     // Enable logs to be sent to Sentry
     enableLogs: true,
+    tracesSampleRate: isProduction ? 0.1 : 1.0,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
