0a3d8adcd25778af050476aab119899f68fd545b
Makon <makon@hey.com>
2026-04-28 15:20:15 -0600

    Disable Sentry wide client source-map upload (#164)

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -58,8 +58,8 @@ export default isSentryEnabled
       // For all available options, see:
       // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-      // Upload a larger set of source maps for prettier stack traces (increases build time)
-      widenClientFileUpload: true,
+      // Keep source map uploads scoped to the default client files to avoid slower Vercel builds.
+      widenClientFileUpload: false,
 
       // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
       // This can increase your server load as well as your hosting bill.
